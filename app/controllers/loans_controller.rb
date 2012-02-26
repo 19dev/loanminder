a5ea6bd309c1471ac692c1b9894abc41dc0e1e7d
Ender <enderyurt@gmail.com>
2012-02-26 11:58:53 +0200

    fb graph adde but failed

--- app/controllers/loans_controller.rb
+++ app/controllers/loans_controller.rb
@@ -23,11 +23,18 @@ class LoansController < ApplicationController
     @loan = Loan.find(params[:id])
      if @loan.payed_deadline < Date.today && !@loan.pay #TODO change with Date.today
        @loan.amount = @loan.amount + (@loan.amount * 0.1)
-       @loan.payed_deadline = Date.today
+       @loan.loan_date = Date.today
        @loan.save
-       provider = User.find_by_provider_and_name('twitter', current_user.name)
-       @twitter = Twitter::Client.new(:oauth_token => provider.token, :oauth_token_secret => provider.secret)
-       @twitter.update("PAY IT!!! #{@loan.payer} until #{@loan.payed_deadline}, it is just #{@loan.amount} TL")
+       #provider = current_user.provider
+        #if provider == 'twitter'
+          provider = User.find_by_provider_and_name('twitter', current_user.name)
+          @twitter = Twitter::Client.new(:oauth_token => provider.token, :oauth_token_secret => provider.secret)
+          @twitter.update("PAY IT!!! #{@loan.payer} until #{@loan.payed_deadline}, it is just #{@loan.amount} TL")
+        #elsif provider == 'facebook'
+          #provider = User.find_by_provider_and_name('facebook', current_user.name)
+          #@facebook = FbGraph::User.me(provider.token)
+          #@facebook.feed!( :message => "loanminder-test-message-#{Time.now.utc}")
+      #end
        #TODO add flash message
        redirect_to loans_path
      else
--- app/mailers/loan_mailer.rb
+++ app/mailers/loan_mailer.rb
@@ -2,6 +2,6 @@ class LoanMailer < ActionMailer::Base
   default from: "loanminder@gmail.com"
   
   def remind_loan(loan)
-    mail(:to => current_user.email, :subject => "Borcun var ode!")
+    mail(:to => current_user.email, :subject => "#{loan.payer} should pay #{loan.amount} TL until #{loan.payed_deadline}")
   end
 end
